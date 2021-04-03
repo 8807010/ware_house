d4569307a433709ca05e4b604f3a111a09a5daac
artiom sokolov <asokolik87@gmail.com>
2021-04-03 17:54:39 +0300

    images for header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import s from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={s.app}>
+
+      <div className={s.cork}>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+  
+
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      </div>
+
+      <div className={s.goodThings}>
+
+      </div>
+
+      <div className={s.nav}>
+
+      </div>
+
+      <div className={s.sevenPots}>
+
+      </div>
+
+      <div className={s.teaOclock}>
+
+      </div>
+
+      <div className={s.rooms}>
+
+      </div>
+
+      <div className={s.ebony}>
+
+      </div>
+
+      <div className={s.shakers}>
+
+      </div>
+
+      <div className={s.authentics}>
+
+      </div>
+
+      <div className={s.backToBasis}>
+
+      </div>
+
+      <footer>
+
+      </footer>
+
     </div>
   );
 }
