596e83291a0f1617de6c2ffb83f9a9a904804515
artiom sokolov <asokolik87@gmail.com>
2021-04-07 13:29:27 +0300

    progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import s from './App.module.scss';
+import rectHeader from "./components/assets/images/rectHeader.png";
 
 function App() {
   return (
+
+    // объекты для CardCollection
+  const data0 = {
+    img: pots,
+    alt: 'pots',
+    title: 'SEVEN POTS'
+  }
+  const data1 = {
+    img: shakers,
+    alt: 'shakers',
+    title: 'SHAKERS'
+  }
+  const data2 = {
+    img: authentics,
+    alt: 'authentics',
+    title: 'AUTHENTICS'
+  }
+
+  // объекты для CardProduct
+  const fact0 = {
+    name: 'Kettle Thermo Pot',
+    price: '274.50 $',
+    span: 'ADD TO FAVS'
+  }
+
+  const fact1 = {
+    name: 'Salt & Pepper  Grinder',
+    price: '75.50 $',
+    span: 'FAVED'
+  }
+
+  const fact2 = {
+    name: 'Generic Oak Bowl',
+    price: '87.49 $',
+    span: 'FAVED copy'
+  }
+
+  // объекты для CardAdvice
+  const record0 = {
+    foto: fotoNOOR,
+    caption: 'BY NOOR JAHAAN',
+    title: 'Tea  O’Clock'
+  }
+
+  const record1 = {
+    foto: fotoAMISHA,
+    caption: 'BY AMISHA PATEL',
+    title: 'Ebony & Ivory'
+  }
+
+  const record2 = {
+    foto: fotoKARINA,
+    caption: 'BY KARINA KAPOOR',
+    title: 'Back to Basics'
+  }
+    <>
     <div className={s.app}>
 
       <div className={s.cork}>
-        <div className={s.rectSmall}>
-          <ul className={s.rectList}>
-            <li className={s.listItem}><img className={s.colorRect}src="./components/assets/images/oneLine/rectYellow.png" alt="yellow"></img></li>
-            <li className={s.listItem}><img className={s.colorRect}src="./components/assets/images/oneLine/rectGreen.png" alt="green"></img></li>
-            <li className={s.listItem}><img className={s.colorRect}src="./components/assets/images/oneLine/rectRed.png" alt="red"></img></li>
-            <li className={s.listItem}><img className={s.colorRect}src="./components/assets/images/oneLine/rectBlue.png" alt="blue"></img></li>
-          </ul>
-          
-          
-          
-          
+        <img className={s.headerFoto} src={rectHeader} alt="header rect"/>
         
-
-        </div>
-        <p>Cork Salt & Pepper 45.50  $</p>
-
+        <h3 className={s.price}>Cork Salt & Pepper 45.50  $</h3>
       </div>
 
 
@@ -34,6 +78,15 @@ function App() {
       </div>
 
       <div className={s.sevenPots}>
+        
+
+
+
+
+
+
+
+
 
       </div>
 
@@ -66,6 +119,7 @@ function App() {
       </footer>
 
     </div>
+    </>
   );
 }
 
